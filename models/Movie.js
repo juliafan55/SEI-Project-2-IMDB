f7d01c65203e3eff1db75d8f018bda57b6e617d3
juliafan55 <juliafan55@gmail.com>
2022-04-26 09:58:48 -0400

    set up movie model

---
+++ models/Movie.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    poster: {
+        type: String,
+        required: [true, "Poster cannot be empty"]
+    },
+    title: {
+        type: String,
+        required: [true, "Title cannot be empty"],
+    },
+    cast: [{
+        type: String,
+    }],
+    synopsis: String,
+    director: {
+        type: String,
+        required: [true, "Director cannot be empty"]
+    },
+    releaseMonth: {
+        type: Number,
+        min: 1,
+        max: 12,
+        required: [true, "Release month cannot be empty"]
+    },
+    releaseDate: {
+        type: Number,
+        min: 1,
+        max: 31,
+        required: [true, "Release date cannot be empty"]
+    },
+    releaseYear: {
+        type: Number,
+        min: 1888,
+        max: 2030,
+        required: [true, "Release year cannot be empty"]
+    },
+})
