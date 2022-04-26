d2fee05482595724325f28c27bb0a93aec418741
anandpatel48 <anandrpatel48@gmail.com>
2022-04-26 11:47:52 -0400

    Create Review Schema

--- models/Review.js
+++ models/Review.js
@@ -1,1 +1,23 @@
 const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        default: 5,
+        min: 1,
+        max: 10,
+        required: [true, "Must have a rating out of 10"]
+    },
+    comment: {
+        type: String,
+        required: [true, "Must have rating explanation"]
+    },
+    movie: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Movie'
+    }
+}, {timestamps: true})
+
+const Review = mongoose.model("Review", reviewSchema)
+
+module.exports = Review
