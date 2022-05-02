8c4296fe46d139cce78c7d5f2337170f0958b107
juliafan55 <juliafan55@gmail.com>
2022-05-02 10:58:59 -0400

    fix image on show page

--- models/index.js
+++ models/index.js
@@ -1,9 +1,5 @@
-// mongoose.connect( process.env.MONGODB_URI || "mongodb+srv://cluster0.yc27i.mongodb.net/myFirstDatabase");
-
 module.exports = {
     Movie: require('./Movie'),
     Review: require('./Review'),
     User: require('./User'),
-    
-
 }
